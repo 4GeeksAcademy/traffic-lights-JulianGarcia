960663c12efc22158d032d24a482294c87b40c9b
juliangarciag05 <juliangarciag05@gmail.com>
2025-10-20 22:34:35 +0000

    termine fucnionalidad

--- src/js/components/Trafficlights.jsx
+++ src/js/components/Trafficlights.jsx
@@ -2,35 +2,56 @@ import React, { useState } from "react";
 
 export const Trafficlights = () => {
 
-    let [counter, setCounter] = useState(0)
-    let [btnGlow, setBtnGlow] = useState("btn-success")
+
+
+    let [color, setColor] = useState("btn-danger")
+    let [colorY, setColorY] = useState("btn-warning")
+    let [colorG, setColorG] = useState("btn-success")
 
 
 
     let change = () => {
+        setColor("btn-danger glow")
 
-        if (counter === 0) {
-            setCounter(1)
-            setBtnGlow("btn-danger")
-        } else {
-            setCounter(0)
-            setBtnGlow("btn-success")
-        }
+        setColorY("btn-warning")
+        setColorG("btn-success")
+
+
     }
+
+    let changeY = () => {
+        setColorY(" btn-warning glow")
+
+        setColor("btn-danger")
+        setColorG("btn-success")
+
+
+
+    }
+
+    let changeG = () => {
+        setColorG("btn-success glow")
+
+        setColor("btn-danger")
+        setColorY("btn-warning")
+
+
+    }
+
 
     return (
         <>
-        <div className="container1"></div>
+            <div className="container1"></div>
             <div className="container d-flex flex-column" >
                 <div className="row">
                     <div className="col-12">
 
-                        <button type="button" className={"btn " + btnGlow} onClick={change}></button>
+                        <button type="button" className={"btn " + color} onClick={change}></button>
                     </div>
                     <div className="col-12">
-                        <button type="button" className={"btn " + btnGlow} onClick={change}></button></div>
+                        <button type="button" className={"btn " + colorY} onClick={changeY}></button></div>
                     <div className="col-12">
-                        <button type="button" className={"btn " + btnGlow} onClick={change}></button>
+                        <button type="button" className={"btn " + colorG} onClick={changeG}></button>
                     </div>
                 </div>
             </div>
